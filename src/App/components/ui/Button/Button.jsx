3e557e7987a85809d2eb2885fd7ae63e056f5eea
tforey <thomas@forey.fr>
2023-01-23 14:35:00 +0100

    Button

---
+++ src/App/components/ui/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button=()=>{
+    return <button className='Button'>tom</button>
+}
+
+/*
+function Button(){
+ return <button className='Button'>tom</button>
+}*/
+
+export default Button;
