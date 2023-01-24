6fc2bcc482840e90b4f14169006d75cf22f8c022
tforey <thomas@forey.fr>
2023-01-24 14:51:07 +0100

    save protype

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,23 +1,38 @@
-import React,{useState} from 'react'
-import style from './Button.module.css'
+import React, { useEffect, useState } from "react";
+import style from "./Button.module.css";
 
 /**
  * un bouton !!!
  * extension nav "React Developper Tools" pour voir les états des état sur le navigateur
  * @returns react component structure
  */
-const Button=(props)=>{
-    const[isClicked, setIsClicked]  = useState(false);//état initial de l'état de l'objet
-    console.log(props);
-    return (
-      <button className={
-                 //isClicked?style.Button+' '+style.clicked:style.Button
-                 `${style.Button}${isClicked?' '+style.clicked:''}`} 
-              onClick={(arg)=>{console.log(arg);
-              props.onButtonClick();
-         }}>
-        {props.children}
-    </button>);
-}
+const Button = (props) => {
+  const [isClicked, setIsClicked] = useState(false); //état initial de l'état de l'objet
+  //hook d'effet de cycle de vie d'une valeur ou de composant
+  useEffect(() => {
+    //si on met pas le if on va tourner en boucle !
+    if (isClicked) {
+      setTimeout(() => {setIsClicked(false);
+    }, 750);
+
+    }
+  }, [isClicked]);
+  console.log(props);
+  return (
+    <button
+      className={
+        //isClicked?style.Button+' '+style.clicked:style.Button
+        `${style.Button}${isClicked ? " " + style.clicked : ""}`
+      }
+      onClick={(arg) => {
+        setIsClicked(true);
+        console.log(arg);
+        props.onButtonClick();
+      }}
+    >
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
